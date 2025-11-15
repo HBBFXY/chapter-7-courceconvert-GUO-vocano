6dbb6e6dd96fdab7d0b4e93120157bcbeac98b60
GUO-vocano <1605776459@qq.com>
2025-11-15 16:53:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-# 在这个文件中编写代码实现题目要求的功能
-import keyword  # 建议使用这个库处理关键字
-reserved_words = set(keyword.kwlist)
+import keyword
 
-# 以下内容自行完成
+# 读取文件
+with open('random_int.py', 'r') as f:
+    content = f.readlines()
+
+# 处理每一行内容
+new_content = []
+for line in content:
+    words = line.split()
+    new_words = []
+    for word in words:
+        # 判断是否为保留字
+        if keyword.iskeyword(word):
+            new_words.append(word)
+        else:
+            # 非保留字的小写字母转为大写
+            new_words.append(word.upper())
+    # 重组行内容
+    new_line = ' '.join(new_words) + '\n'
+    new_content.append(new_line)
+
+# 保存到新文件
+with open('random_int_converted.py', 'w') as f:
+    f.writelines(new_content)
